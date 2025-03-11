2efc46da281cb1325266267fe7791afb7b03ad5e
Oscar Gordillo <oscar_andres16@hotmail.com>
2025-03-11 22:06:31 +0100

    Improved schema and sql to include song-vocab.

--- lang-portal/backend-flask/sql/setup/insert_study_activities.sql
+++ lang-portal/backend-flask/sql/setup/insert_study_activities.sql
@@ -1,3 +1,4 @@
 INSERT INTO study_activities (name, url, preview_url) VALUES 
 ('Typing Tutor', 'http://localhost:8080', '/assets/study_activities/typing-tutor.png'),
-('Writing Practice', 'http://localhost:8080/writing', '');
+('Writing Practice', 'http://localhost:8080/writing', '/assets/study_activities/writing-practice.png'),
+('Song Vocabulary', 'http://localhost:8000', '/assets/study_activities/song-vocab.png');
